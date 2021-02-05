348ec4635094be9f11c6deb39dd9addee2f6e7ac
Hale <aspire_8@163.com>
2021-02-05 16:15:11 +0800

    变量类型-添加注释-变量范围：类变量，实例变量，局部变量

--- src/java/cn/hale/com/VariableType.java
+++ src/java/cn/hale/com/VariableType.java
@@ -5,7 +5,11 @@ package cn.hale.com;
  * @date 2021-02-05
  **/
 public class VariableType {
+
+    static int allClicks = 0;  // 类变量
+    int ObjClick = 0;       // 实例变量
     public static void main(String[] args) {
+        int InnerClick = 0;  // 局部变量
 //        变量类型:
 //        声明三个int整型数： a b c
         int a,b,c;
@@ -19,6 +23,13 @@ public class VariableType {
         double pi = 3.1415926;
 //        声明变量x的值是字符 x
         char x = 'x';
+//        java 支持的变量类型有：
+/*
+
+        类变量：独立于方法之外的变量，用static修饰
+        实例变量：独立于方法之外的变量，没有static修饰
+        局部变量： 类的方法中的变量
+*/
 
     }
 }
