2c8948b7fc381ba1da98a4832ed1aaaa8de446b8
Hale <aspire_8@163.com>
2021-02-05 15:40:57 +0800

    变量类型

---
+++ src/java/cn/hale/com/VariableType.java
@@ -0,0 +1,20 @@
+package cn.hale.com;
+
+/**
+ * @author HaleLv
+ * @date 2021-02-05
+ **/
+public class VariableType {
+    public static void main(String[] args) {
+//        变量类型:
+//        声明三个int整型数： a b c
+        int a,b,c;
+//        声明三个整数并赋予初值
+        int d = 3, e = 4, f = 5;
+        byte z = 22;
+        String s = "hale";
+        double pi = 3.1415926;
+        char x = 'x';
+
+    }
+}
