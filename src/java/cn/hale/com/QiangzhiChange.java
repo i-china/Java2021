d9526e56ee42cd13232e769306666c7653da4959
Hale <aspire_8@163.com>
2021-02-05 15:26:45 +0800

    强制类型转换： (type)int ！

---
+++ src/java/cn/hale/com/QiangzhiChange.java
@@ -0,0 +1,13 @@
+package cn.hale.com;
+
+/**
+ * @author HaleLv
+ * @date 2021-02-05
+ **/
+public class QiangzhiChange {
+    public static void main(String[] args) {
+        int i = 123;
+        byte b = (byte)i;
+        System.out.println(b);
+    }
+}
