8ab9fef353f714852a3232ce6419511567a9b266
Hale <aspire_8@163.com>
2021-02-05 16:44:07 +0800

    to do it

--- src/java/cn/hale/com/XiuShi.java
+++ src/java/cn/hale/com/XiuShi.java
@@ -5,5 +5,11 @@ package cn.hale.com;
  * @date 2021-02-05
  **/
 public class XiuShi {
-
+// 访问控制修饰符
+    private boolean myFlag;
+    static final double weeks = 8.2;
+    protected static final int BOXWITH = 32;
+    public static void main(String[] args){
+//        方法体
+    }
 }
