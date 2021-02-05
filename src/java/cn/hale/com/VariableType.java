b49706bcf11387607560c513babb7f947314876d
Hale <aspire_8@163.com>
2021-02-05 15:54:47 +0800

    变量类型-添加注释

--- src/java/cn/hale/com/VariableType.java
+++ src/java/cn/hale/com/VariableType.java
@@ -11,10 +11,16 @@ public class VariableType {
         int a,b,c;
 //        声明三个整数并赋予初值
         int d = 3, e = 4, f = 5;
+//        声明并初始化z
         byte z = 22;
+//        声明并初始化字符串s
         String s = "hale";
+//        声明双精度浮点型变量 pi
         double pi = 3.1415926;
+//        声明变量x的值是字符 x
         char x = 'x';
 
     }
 }
+
+
