df531cec3e74a5b1746940172e4d161c9e660331
Hale <aspire_8@163.com>
2021-02-06 10:19:45 +0800

    to do it

---
+++ src/java/cn/data/com/SpringBoot.java
@@ -0,0 +1,11 @@
+package cn.data.com;
+
+/**
+ * @author HaleLv
+ * @date 2021-02-06
+ **/
+public class SpringBoot {
+    public static void main(String[] args) {
+        System.out.println("Today begin SpringBoot");
+    }
+}
