cba0c2ee68d854140f47f948478ad5f9c86014d9
Hale <aspire_8@163.com>
2021-02-07 21:29:20 +0800

    to do it

--- src/java/cn/data/com/SuperDemo.java
+++ src/java/cn/data/com/SuperDemo.java
@@ -5,19 +5,42 @@ package cn.data.com;
  * @date 2021-02-07
  **/
 public class SuperDemo {
-
+    public static void main(String[] args) {
+        FatherSuper sf = new FatherSuper(name);
+//        sf.age = 24;
+    }
 }
 
 class FatherSuper {
-    protected String name;
+    protected final String name;
+//    public final int age = 23;
+    public final int age;
+
+    public FatherSuper(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
     public String hello(){
         return "hi " + name;
+    }
+    public final String hi(){
+//        age = 23;
+        return "hi final" + name;
     }
 }
 
 class SonSuper extends FatherSuper {
+    SonSuper() {
+        super(name);
+    }
+
     @Override
     public String hello(){
         return super.hello();
     }
+
+    /*public String hi(){
+
+    }*/
 }
