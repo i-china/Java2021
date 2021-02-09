342bb4166200516442fe30a8f505f99f2cef3255
Hale <aspire_8@163.com>
2021-02-09 17:10:34 +0800

    :tada: Math-Random

--- src/java/com/tools/cn/MathDemo.java
+++ src/java/com/tools/cn/MathDemo.java
@@ -1,13 +1,46 @@
 package com.tools.cn;
 
+import com.sun.media.sound.SoftTuning;
+
+import java.util.Random;
+
 /**
  * @author HaleLv
  * @date 2021-02-09
  **/
 public class MathDemo {
     public static void main(String[] args) {
-        One a = new One();
+//        One a = new One();
+        RandomDemo rd = new RandomDemo();
+        System.out.println(rd);
+        RandomFor rf = new RandomFor();
 
+        Random a = new Random(123);
+        System.out.println(" random is " + a.nextInt());
+
+    }
+}
+
+class RandomFor{
+    RandomFor(){
+        Random r = new Random(123);
+        for(int i = 0; i < 10; i++){
+            System.out.println(r.nextInt(100));
+        }
+    }
+}
+
+class RandomDemo {
+    RandomDemo(){
+        Random r = new Random();
+        int a =   r.nextInt(100);
+//        r.nextBytes();
+        long l = r.nextLong();
+        boolean bl = r.nextBoolean();
+        float f = r.nextFloat();
+        double d = r.nextDouble();
+        int ii = r.nextInt(3);
+        System.out.println(a + " " + l + " " + bl + "  " + r + " " + d  + " " + ii);
     }
 }
 
