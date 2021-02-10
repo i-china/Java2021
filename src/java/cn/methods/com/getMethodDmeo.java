bfafab0d1f1e68d2827585b311c0d423072110b8
Hale <aspire_8@163.com>
2021-02-10 20:19:46 +0800

    :tada: Class Method getMethod Integer i =  Integer.class.getMethod("parseInt","123")

--- src/java/cn/methods/com/getMethodDmeo.java
+++ src/java/cn/methods/com/getMethodDmeo.java
@@ -9,9 +9,18 @@ import java.lang.reflect.Method;
 public class getMethodDmeo {
     public static void main(String[] args) throws Exception {
         Person5 p5 = new Person5();
+        Student5 s5 = new Student5();
     }
 }
 
+class Student5{
+    public Student5() throws Exception {
+        String s = "hello world";
+        Method m = Integer.class.getMethod("parseInt", String.class);
+        Integer i = (Integer) m.invoke(null,"12345");
+        System.out.println(i);
+    }
+}
 
 class Person5{
     Person5() throws Exception {
