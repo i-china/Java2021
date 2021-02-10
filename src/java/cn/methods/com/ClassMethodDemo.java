5d46742c8deca2c09faed3ac91dff6fcf5868f92
Hale <aspire_8@163.com>
2021-02-10 20:07:20 +0800

    :tada: Class Method getMethod getDeclaredMethod

--- src/java/cn/methods/com/ClassMethodDemo.java
+++ src/java/cn/methods/com/ClassMethodDemo.java
@@ -5,4 +5,26 @@ package cn.methods.com;
  * @date 2021-02-10
  **/
 public class ClassMethodDemo {
+    public static void main(String[] args) throws Exception {
+        Class c = Student4.class;
+        System.out.println(c.getMethod("getScore",String.class));
+        System.out.println(c.getMethod("getName"));
+//        System.out.println(c.getMethod("getGrade"));
+        System.out.println(c.getDeclaredMethod("getGrade", int.class));
+    }
+}
+
+class Person4{
+    public String getName(){
+        return "Person";
+    }
+}
+
+class Student4 extends Person4{
+    public int getScore(String type){
+        return 99;
+    }
+    private int getGrade(int year){
+        return 1;
+    }
 }
