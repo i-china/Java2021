acf327f78488be0b8e7bd5f1f4568e061f592010
Hale <aspire_8@163.com>
2021-02-10 19:58:42 +0800

    :tada: Class class set

---
+++ src/java/cn/methods/com/ClassMethodDemo.java
@@ -0,0 +1,8 @@
+package cn.methods.com;
+
+/**
+ * @author HaleLv
+ * @date 2021-02-10
+ **/
+public class ClassMethodDemo {
+}
--- src/java/com/data/cn/FieldSetDemo.java
+++ src/java/com/data/cn/FieldSetDemo.java
@@ -1,8 +1,33 @@
 package com.data.cn;
 
+
+import java.lang.reflect.Field;
+
 /**
  * @author HaleLv
  * @date 2021-02-10
  **/
-public class FieldSetDemo {
+
+public class FieldSetDemo{
+    public static void main(String[] args) throws Exception {
+        Person3 p3 = new Person3("hale");
+        System.out.println(p3.getName());
+        Class c = p3.getClass();
+        Field f = c.getDeclaredField("name");
+        f.setAccessible(true);
+        f.set(p3,"Hadoop ");
+        System.out.println(p3.getName());
+
+    }
 }
+
+class Person3{
+    private String name;
+    public Person3(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
+
