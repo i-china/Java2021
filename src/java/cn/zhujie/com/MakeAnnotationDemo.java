21e79da6d8470d376ab74ea724b5b7a81c7f66a5
Hale <aspire_8@163.com>
2021-02-12 12:04:59 +0800

    :fire:   @interface

--- src/java/cn/zhujie/com/MakeAnnotationDemo.java
+++ src/java/cn/zhujie/com/MakeAnnotationDemo.java
@@ -1,8 +1,48 @@
 package cn.zhujie.com;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author HaleLv
  * @date 2021-02-12
  **/
 public class MakeAnnotationDemo {
+    public static void main(String[] args) {
+
+    }
+
+    public @interface Report{
+        int type() default 0;
+        String level() default "info";
+        String value() default "";
+    }
+    @Target(ElementType.METHOD)
+    public @interface ReportOne{
+        int type() default 0;
+        String level() default "info";
+        String value() default "";
+    }
+    @Target({
+            ElementType.METHOD,
+            ElementType.FIELD
+    })
+    public @interface AnnoOne{
+        int type() default 0;
+        String level() default "info";
+        String value() default  "";
+    }
+}
+
+class AnnotationDemos{
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Report{
+        int type() default 0;
+        String level() default "error";
+        String value() default "";
+    }
+
+
 }
