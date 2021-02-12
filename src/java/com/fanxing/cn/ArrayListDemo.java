3868dc638bbd886536293032d05da6f3a85a2430
Hale <aspire_8@163.com>
2021-02-12 13:48:52 +0800

    :fire: Generic

---
+++ src/java/com/fanxing/cn/ArrayListDemo.java
@@ -0,0 +1,20 @@
+package com.fanxing.cn;
+
+import java.util.ArrayList;
+
+/**
+ * @author HaleLv
+ * @date 2021-02-12
+ **/
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add("Hello");
+    }
+    private Object[] array;
+    private int size;
+    public void add(Object e){}
+    public void remove(int index){}
+    public void get(int index){}
+
+}
