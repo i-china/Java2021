15ca9f2730f3cbf7ed395db260eac69a5e77e579
Hale <aspire_8@163.com>
2021-02-15 07:26:25 +0800

    :tada: IO : File

---
+++ src/java/com/iofile/cn/IODemo.java
@@ -0,0 +1,16 @@
+package com.iofile.cn;
+
+import java.io.File;
+
+/**
+ * @author HaleLv
+ * @date 2021-02-15
+ **/
+public class IODemo {
+    public static void main(String[] args) {
+        File f = new File("E:\\Java2021\\io.exe");
+        System.out.println(f);
+    }
+}
+
+
