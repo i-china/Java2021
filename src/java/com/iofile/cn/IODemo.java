7a099fe716708fdeda57b27167c8c4217ce0890a
Hale <aspire_8@163.com>
2021-02-15 07:42:55 +0800

    :tada: IO : File

--- src/java/com/iofile/cn/IODemo.java
+++ src/java/com/iofile/cn/IODemo.java
@@ -1,15 +1,55 @@
 package com.iofile.cn;
 
+import com.sun.media.sound.SoftTuning;
+
 import java.io.File;
+import java.io.IOException;
 
 /**
  * @author HaleLv
  * @date 2021-02-15
  **/
 public class IODemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         File f = new File("E:\\Java2021\\io.exe");
-        System.out.println(f);
+//        System.out.println(f);
+//        FileDemo fd = new FileDemo();
+//        DemoFile df = new DemoFile();
+        CreateFileDemo cfd = new CreateFileDemo();
+    }
+}
+
+class CreateFileDemo{
+    CreateFileDemo() throws IOException {
+        File f = new File("E:\\Java2021\\newFile.txt");
+        File f1 = new File("E:\\test.txt");
+        if(f1.createNewFile()){
+            System.out.println("ok");
+        }
+    }
+}
+
+
+class DemoFile{
+    DemoFile(){
+        File f = new File("E:\\Java2021");
+        File f1 = new File("E:\\java2021");
+        File f2 = new File("E:\\Java2021\\io.exe");
+        File d1 = new File("E:\\Java2021");
+        System.out.println(f.canRead());
+        System.out.println(d1.isDirectory());
+        System.out.println(f2.isFile());
+        System.out.println(f.isFile());
+        System.out.println(f1.isFile());
+    }
+}
+class FileDemo{
+    FileDemo() throws IOException {
+        File f = new File("E:\\Java2021");
+        System.out.println(f.getPath());
+        System.out.println(f.getAbsolutePath());
+        System.out.println(f.getCanonicalPath());
+        System.out.println(File.separator);
     }
 }
 
