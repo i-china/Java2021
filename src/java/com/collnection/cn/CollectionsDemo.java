6c3e77e8e539d94ebb4e51f0ee5b060bf57454fc
Hale <aspire_8@163.com>
2021-02-14 12:48:38 +0800

    :tada: 情人节快乐!

--- src/java/com/collnection/cn/CollectionsDemo.java
+++ src/java/com/collnection/cn/CollectionsDemo.java
@@ -1,5 +1,9 @@
 package com.collnection.cn;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author HaleLv
  * @date 2021-02-13
@@ -7,12 +11,19 @@ package com.collnection.cn;
 
 public class CollectionsDemo{
     public static void main(String[] args) {
+        Collections1 c1 = new Collections1();
 
     }
 }
 
 class Collections1{
     Collections1(){
-
+        List<String> list = new ArrayList<>();
+        list.add("Admin");
+        list.add("root");
+        list.add("hadoop");
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
     }
 }
---
+++ src/java/com/collnection/cn/today2021.java
@@ -0,0 +1,11 @@
+package com.collnection.cn;
+
+/**
+ * @author HaleLv
+ * @date 2021-02-14
+ **/
+public class today2021 {
+    public static void main(String[] args) {
+        System.out.println("情人节快乐！");
+    }
+}
