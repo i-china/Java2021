6a4c7654eefbf4d1fbeccbdb933a8eb6f4e064d7
aspire_8 <aspire_8@163.com>
2021-03-01 15:46:35 +0800

    ":zap: File"

---
+++ src/java/com/thread/cn/ThreadDemo.java
@@ -0,0 +1,13 @@
+package com.thread.cn;
+
+/**
+ * @Author: HaleLv
+ * @Date: 2021-03-01
+ * @ProjectName Java2021
+ */
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+
+    }
+}
